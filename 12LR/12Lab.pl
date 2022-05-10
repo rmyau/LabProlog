e05617ed139bf23df414b865191b72b2e9e18eb6
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-10 15:51:13 +0300

    12 задание 12лр произведение делителей, сумма цифр которых < суммы цифр исходного числа

--- 12LR/12Lab.pl
+++ 12LR/12Lab.pl
@@ -16,3 +16,19 @@ sumDelDown(_,0,Sum,Sum):-!.
 sumDelDown(X,Del,Sum,CurSum):-
     (0 is X mod Del,prost(Del), NewSum is CurSum+Del; NewSum is CurSum),
     D1 is Del-1, sumDelDown(X,D1,Sum,NewSum).
+
+% 12 найти произведение таких делителей числа, сумма цифр которых
+% меньше,чем сумма цифр исходного числа
+
+%сумма цифр
+sumCifr(X,Sum):- sumCifrDown(X,Sum,0).
+sumCifrDown(0,Res,Res):-!.
+sumCifrDown(X,Sum,CurSum):- X1 is X mod 10, NowSum is CurSum+X1,
+    X2 is X div 10, sumCifrDown(X2,Sum,NowSum).
+
+multDel(X,Res):- multDel(X,X,Res,1),!.
+multDel(_,0,Res,Res):-!.
+multDel(X,Del,Res,CurMult):-
+    (0 is X mod Del,sumCifr(Del,Y),sumCifr(X,Z),Y<Z,
+    NewMult is CurMult*Del; NewMult is CurMult),D1 is Del-1,
+    multDel(X,D1,Res,NewMult).
