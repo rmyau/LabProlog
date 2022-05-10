da79c417d21461f34097b4ab6564601b76d55b35
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-10 14:48:16 +0300

    рекурсия вниз сумма простых делителей

--- 12LR/12Lab.pl
+++ 12LR/12Lab.pl
@@ -10,3 +10,9 @@ sumDelUp(_,0,0):-!.
 sumDelUp(X,Del,Sum):-D1 is Del-1, sumDelUp(X,D1,NewSum),
     (0 is X mod Del,prost(Del), Sum is NewSum+Del; Sum is NewSum).
 
+%11.2 рекурсия вниз
+sumDelDown(X,Sum):-sumDelDown(X,X,Sum,0),!.
+sumDelDown(_,0,Sum,Sum):-!.
+sumDelDown(X,Del,Sum,CurSum):-
+    (0 is X mod Del,prost(Del), NewSum is CurSum+Del; NewSum is CurSum),
+    D1 is Del-1, sumDelDown(X,D1,Sum,NewSum).
