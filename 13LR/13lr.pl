f8054c9104242da50da88c285bd60c2e517cf0db
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-18 12:33:21 +0300

    Добавлено 12 задание 13ЛР

--- 13LR/13lr.pl
+++ 13LR/13lr.pl
@@ -4,7 +4,7 @@ readList(I, [X|T]) :- write("input - "),read(X), I1 is I - 1, readList(I1, T).
 write_list([]) :- !.
 write_list([X|T]) :- write(X), nl, write_list(T).
 
-%1.37 вывести индексы элементов, которые меньше своего левого соседа и их количество
+%11.37 вывести индексы элементов, которые меньше своего левого соседа и их количество
 
 lessLeft([H|Tail],Count):-lessLeft(H,Tail,0, Count,1).
 lessLeft(_,[],Count,Count,_):-!.
@@ -13,4 +13,20 @@ lessLeft(LeftEl,[H|Tail],CountNow,Count, Ind):-
     NextInd is Ind+1, lessLeft(H,Tail,NewCount,Count,NextInd).
 
 task11:-write("Input lenght of list: "), read(Count),readList(Count,List),
-    lessLeft(List,X), write("Number of elements that are less than the left: "),write(X).
+    lessLeft(List,X), write("Number of elements that are less than the left: "),write(X), nl.
+
+%12.43 найти количество минимальных элементов 
+minEl([H|Tail],Min):- minEl(Tail,H,Min).
+minEl([],Min,Min):-!.
+minEl([H|Tail],MinNow,Min):- 
+    (H<MinNow, NewMin is H,!; NewMin is MinNow),
+    minEl(Tail,NewMin,Min).
+
+countMin(List,Count):- minEl(List,Min),countMin(List,Min,0,Count).
+countMin([],_,Count,Count):-!.
+countMin([H|Tail],Min,CountNow,Count):- 
+    (H is Min, CountMin is CountNow+1,!;CountMin is CountNow),
+    countMin(Tail,Min,CountMin,Count).
+
+task12:-write("Input lenght of list: "), read(Count),readList(Count,List),
+    countMin(List,C),write("Number of min: "), write(C), nl.
