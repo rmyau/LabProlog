aea9b8dbee83437f8ffdaa7dc2d412c9b7a35660
rmyau <91686883+rmyau@users.noreply.github.com>
2022-05-24 20:33:07 +0300

    task 1.2 14lr

--- 14LR/14Lab.pl
+++ 14LR/14Lab.pl
@@ -8,4 +8,20 @@ write_str([H|T]):-put(H),write_str(T).
 task11 :- read_str(A,N),write_str(A),write(', '),write_str(A),write(', '),
 		write_str(A),write(', '),write(N).
 
+count_words(A,K):-count_words(A,0,K).
+count_words([],K,K):-!.
+count_words(A,I,K):-skip_space(A,A1),get_word(A1,Word,A2),Word \=[],I1 is I+1,count_words(A2,I1,K),!.
+count_words(_,K,K).
+
+skip_space([32|T],A1):-skip_space(T,A1),!.
+skip_space(A1,A1).
+
+get_word([],[],[]):-!.
+get_word(A,Word,A2):-get_word(A,[],Word,A2).
+get_word([],Word,Word,[]).
+get_word([32|T],Word,Word,T):-!.
+get_word([H|T],W,Word,A2):-append(W,[H],W1),get_word(T,W1,Word,A2).
+
+%Дана строка. Найти количество слов.
+task12 :- read_str(A,_), count_words(A,K), write(K).
 
